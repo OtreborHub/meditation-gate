1b705c5b74f3718ac079a1ff8384c261c18f9d19
QuestionSigns <zito.roberto94@gmail.com>
2023-09-29 12:51:51 +0200

    helmet usage

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css';
 import SessionDashboard from './view/SessionDashboard';
 import Home from './view/Home';
+import { Helmet } from "react-helmet";
 import initFirebase from "./utils/firebase";
 
 export default function App() {
@@ -14,6 +15,10 @@ export default function App() {
         <Route path="/" index element={<Home />} />
         <Route path="/dashboard" element={<SessionDashboard />} />
       </Routes>
+      <Helmet>
+        <title>Meditation Gate</title>
+        {/* Aggiungere altri metatag qui */}
+      </Helmet>
     </BrowserRouter>
   );
 }
